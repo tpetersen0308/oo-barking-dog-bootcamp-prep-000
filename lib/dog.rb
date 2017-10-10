ae05d2a3583e559dd9c55c796fa61ccf17b81516
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-10 22:24:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def initialize(dog_name)
+    @name = dog_name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def bark
+    puts "Woof!"
+  end
+end
